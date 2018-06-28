d8333ecb51b2d1f8417fb48903ef5a7089d10b08
Jeffedibo <jeffedibo@gmail.com>
2018-06-28 16:55:27 +0300

    Add files via upload

---
+++ midtermexam/ExamTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermexam;
+
+/**
+ *
+ * @author DIBO
+ */
+public class ExamTest {
+    String name="James";
+    double height=1.0;
+    double weight=60.5;
+    
+ public static void main(String[] args){
+     
+     double height=1.0;
+     double weight=60.5;
+     
+     
+     
+     
+    
+     
+     
+ }
+public static double getBMI(double height, double weight){
+    
+    double BMI= weight/height;
+    
+    return BMI;
+    
+    
+    
+
+}
+    
+    
+  
+     
+    
+    
+    
+    
+}
